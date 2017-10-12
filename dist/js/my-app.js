76c47ebb2eea8623a4e65bb0cba5b6611a8d66c1
Bruno R <shinfujiwara@gmail.com>
2017-10-12 12:59:33 +0100

    business section

--- dist/js/my-app.js
+++ dist/js/my-app.js
@@ -6,43 +6,54 @@ var $$ = Dom7;
 
 // Add view
 var mainView = myApp.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
 
-// Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
+var businessArticles;
+var businessList = '#business_list';
+
+myApp.onPageInit('business', function (page) {
+
+    if (businessArticles === undefined) {
+
+        $$.getJSON('https://newsapi.org/v1/articles?source=business-insider&sortBy=top&apiKey=7fc906d8914b4b03a009f74688717037',
+
+            function (data) {
+
+                // Storing JSON articles to be used wherever necessary
+                businessArticles = data.articles;
+
+                // Rendering a list with the results
+                displayArticles(businessArticles, businessList);
+
+            },
+
+            function (error) {
+                console.log('Error getting JSON!', error)
+            })
+    } else {
+        displayArticles(businessArticles, businessList)
+    }
 });
 
-// Generate dynamic page
-var dynamicPageIndex = 0;
-function createContentPage() {
-	mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-	return;
+
+// Renders list of articles to be displayed
+function displayArticles(articles, listId) {
+
+    $$.each(articles, function (i) {
+        $$(listId).append(`
+                <li>
+                <a href="#" class="item-link item-content">
+                  <div class="item-media"><img src="${articles[i].urlToImage}" width="100"></div>
+                  <div class="item-inner">
+                    <div class="item-title-row">
+                      <div class="item-title">${articles[i].title}</div>
+                    </div>
+                    <div class="item-subtitle">${articles[i].author}</div>
+                    <div class="item-text">${articles[i].description}</div>
+                  </div>
+                </a>
+              </li>`);
+
+    });
 }
